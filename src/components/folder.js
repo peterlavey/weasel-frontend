cce2a1c15e664f79361e57893ea7a94153746f9a
Pedro Cisternas <pedro.cisternas@siigroup.cl>
2018-03-06 10:54:48 -0300

    Se cambia componente folder por variable

--- src/components/folder.js
+++ src/components/folder.js
@@ -1,30 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import {getFolder} from "../actions/actions";
 
-class Folder extends Component {
-    constructor(props){
-        super(props);
-    }
-
-    render() {
-        console.log(this.props.folders);
-        if(!this.props.folders){
+const Folder = ({folders, getFolders})=> {
+        console.log(folders);
+        if(!folders){
             return <h1>Sin carpetas</h1>
         }
 
-        const folderComponentList = this.props.folders.map((folder, index)=> {
+        const folderComponentList = folders.map((folder, index)=> {
             return (
                 <div key={index} className="row">
-                    <div className="col-md-10" onClick={()=> this.props.getFolders(folder)}>
+                    <div className="col-md-10" onClick={()=> getFolders(folder)}>
                         <h3>
                             <p>{folder}</p>
                         </h3>
                     </div>
 
                     <div className="col-md-2" align="right">
-                        <i className="fa fa-pencil col-md-4 grey fa-1" onClick={()=> this.props.openEditModal()}></i>
-                        <i className="fa fa-trash col-md-4 grey fa-1" onClick={()=> this.props.openConfirm()}></i>
+                        <i className="fa fa-pencil col-md-4 grey fa-1" onClick={()=> this.openEditModal()}></i>
+                        <i className="fa fa-trash col-md-4 grey fa-1" onClick={()=> this.openConfirm()}></i>
                     </div>
                     <hr/>
                 </div>
@@ -36,8 +31,7 @@ class Folder extends Component {
                 {folderComponentList}
             </div>
         )
-    }
-}
+};
 
 const mapStateToProps = state => {
     return {
